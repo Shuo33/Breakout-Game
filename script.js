f31c8e9977fc353afd16a023791a2264f37df508
shuo xu <shuo.xu@2021.icam.fr>
2025-01-10 09:13:08 +0800

    update: create JS plan

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
 
+const canvas = document.getElementById('canvas'); 
+const ctx = document.getElementById('2d'); 
+
+// Create contaxt: create ball props
+const ball = {
+    x: canvas.width / 2, 
+    y: canvas.height / 2, 
+    size: 10, 
+    speed: 4, 
+    dx: 4, 
+    dy: -4
+}
+
+
+
+
 
 
 // Show & close the rules
@@ -10,3 +26,4 @@ rulesBtn.addEventListener('click', () =>
 
 closeBtn.addEventListener('click', () =>
     rules.classList.remove('show'));
+
